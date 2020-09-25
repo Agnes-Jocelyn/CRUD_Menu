4ad608f79e404d0c3e3ce25547bc2b3d381130bc
Agnes Jocelyn <Agnesjocelyn95@gmail.com>
2020-09-25 22:56:55 +0700

    Intitial Project

---
+++ Query.php
@@ -0,0 +1,56 @@
+<?php
+
+include 'Process.php';
+
+$id = 0;
+$name = '';
+$stock = '';
+$price = '';
+$update = false;
+
+if (isset($_POST['save'])) {
+    $name = $_POST['name'];
+    $stock = $_POST['stock'];
+    $price = $_POST['price'];
+
+    mysqli_query(
+        $mysqli,
+        "INSERT INTO menu ( name, stock, price) VALUES ('$name','$stock','$price') "
+    );
+
+    header('location:Index.php');
+}
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    mysqli_query($mysqli, "DELETE from menu WHERE id=$id");
+
+    header('location:Index.php');
+}
+
+if (isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $update = true;
+    $result = mysqli_query($mysqli, "SELECT * from menu WHERE id=$id");
+    print_r($result);
+
+    if ($result->num_rows) {
+        $row = $result->fetch_array();
+        $name = $row['name'];
+        $stock = $row['stock'];
+        $price = $row['price'];
+    }
+}
+
+if (isset($_POST['update'])) {
+    $id = $_POST['update'];
+    $name = $_POST['name'];
+    $stock = $_POST['stock'];
+    $price = $_POST['price'];
+
+    mysqli_query(
+        $mysqli,
+        "UPDATE menu SET name='$name', stock='$stock', price='$price' WHERE id=$id"
+    );
+    // header('location:index.php');
+}
